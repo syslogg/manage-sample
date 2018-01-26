e978750b1e8c64a6cdd3f7e42ac5ea3596a4519b
Hendy Rodrigues F. Silva <mr.syslogg@gmail.com>
2018-01-26 19:09:08 -0300

    Created styles

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,10 +3,11 @@ import { Page } from "ui/page";
 
 @Component({
   selector: "my-app",
-  templateUrl:'./app.component.xml'
+  templateUrl:'./app.component.xml',
+  styleUrls: ['styles/login-common.css','styles/login.css']
 })
 export class AppComponent {
-  
+
   constructor(private _page : Page) { }
 
   ngOnInit() {
